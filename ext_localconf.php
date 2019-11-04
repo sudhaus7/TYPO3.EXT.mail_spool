71e3e89c71fda492beb19ec8483d04608af158d5
Frank Berger <fberger@b-factor.de>
2019-11-04 13:41:51 +0100

    disable autosetup if there is a spool configuration available

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,15 +4,16 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['MAIL']['spool'])) {
+  // Merge our configuration with the core mail configuration.
+  $GLOBALS['TYPO3_CONF_VARS']['MAIL'] += (array) unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
-// Merge our configuration with the core mail configuration.
-$GLOBALS['TYPO3_CONF_VARS']['MAIL'] += (array) unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-
-// If transport_spool is not disabled, we set the transport to our spool transport,
-// and set transport_real to transport.
-if (!empty($GLOBALS['TYPO3_CONF_VARS']['MAIL']['spool'])) {
-    if (empty($GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_real'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_real'] = $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'];
-    }
-    $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = \SUDHAUS7\MailSpool\Mail\SpoolTransport::class;
+  // If transport_spool is not disabled, we set the transport to our spool transport,
+  // and set transport_real to transport.
+  if (!empty($GLOBALS['TYPO3_CONF_VARS']['MAIL']['spool'])) {
+      if (empty($GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_real'])) {
+          $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_real'] = $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'];
+      }
+      $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = \SUDHAUS7\MailSpool\Mail\SpoolTransport::class;
+  }
 }
